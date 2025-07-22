70c36f67b732500248fa213b6f03ea5673961d61
T <tiseolayinka@gmail.com>
2025-07-22 01:17:16 +0100

    Version b3

--- Launcher/obj/Debug/net8.0-windows/Launcher.AssemblyInfo.cs
+++ Launcher/obj/Debug/net8.0-windows/Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47199bb99822742604b10daef76f700f3c23986b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c944f3e3ae48fc47f08c21df10a73cb130add7bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
