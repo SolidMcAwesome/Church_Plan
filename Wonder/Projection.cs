1167570fdad16312205f7265a6fe5fa8b95a3ec6
T <tiseolayinka@gmail.com>
2025-07-23 21:38:06 +0100

    Revert "v4 - Music offically added with playlists. Fully functional"
    This reverts commit 43644193e8190aaf00a9b8049339960f15c9b936.

--- Wonder/Projection.cs
+++ Wonder/Projection.cs
@@ -69,20 +69,10 @@ namespace Wonder
         public void Censor()
         {
             pbxCensor.Visible = !pbxCensor.Visible;
-            pbxCensor.BringToFront();
-        }
-        public bool CensorVisibility()
-        {
-            return pbxCensor.Visible;
         }
         public void Title()
         {
             pbxTitle.Visible = !pbxTitle.Visible;
-            
-        }
-        public bool TitleVisibility()
-        {
-            return pbxTitle.Visible;
         }
     }
 }
